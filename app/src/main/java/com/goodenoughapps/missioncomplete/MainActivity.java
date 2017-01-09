9b224ea83445de312e693223f293905ff57ad6cb
tpatel1781 <tpatel1781@gmail.com>
2017-01-08 22:19:31 -0500

    Add new task FAB

--- app/src/main/java/com/goodenoughapps/missioncomplete/MainActivity.java
+++ app/src/main/java/com/goodenoughapps/missioncomplete/MainActivity.java
@@ -1,13 +1,26 @@
 package com.goodenoughapps.missioncomplete;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton addTasksFAB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addTasksFAB = (FloatingActionButton) findViewById(R.id.add_task_fab);
+        addTasksFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Add Task FAB", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
